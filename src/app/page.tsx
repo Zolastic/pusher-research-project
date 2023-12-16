2fd05cbc64779fe117d0e4cf1e93a22b1f5c17d2
Zolastic <honhattienlul@gmail.com>
2023-12-16 17:07:31 +0800

    feat: set up shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 import { CreatePost } from "~/app/_components/create-post";
+import { Input } from "~/components/ui/input";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
@@ -10,7 +11,9 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-slate-100">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 "></div>
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <Input placeholder="Search" />
+      </div>
     </main>
   );
 }
