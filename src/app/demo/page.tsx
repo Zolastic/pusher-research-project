4e0d9bca656fbac6b37a64c10d0853e6de418a5f
Zolastic <honhattienlul@gmail.com>
2023-12-17 18:11:51 +0800

    feat: set up homepage

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <>
+      <main className="min-h-screen bg-slate-100"></main>
+    </>
+  );
+};
+
+export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import { CreatePost } from "~/app/_components/create-post";
-import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
@@ -12,7 +12,26 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-slate-100">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <Input placeholder="Search" />
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-center text-4xl font-bold">
+            Welcome to our{" "}
+            <a
+              href="https://pusher.com/"
+              className="text-[#5F30E2] hover:underline"
+            >
+              Pusher
+            </a>{" "}
+            research project!
+          </h1>
+          <p className="mt-4 text-center">
+            This is a showcase of tRPC with Next.js, Prisma and Pusher.
+          </p>
+          <Link href={`/demo`} className="mt-5">
+            <Button variant={"link"} className="bg-[#300D4F] text-muted">
+              Demo
+            </Button>
+          </Link>
+        </div>
       </div>
     </main>
   );
