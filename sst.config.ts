a076ae86b5cd540c26a976276755e0306179df3c
Zolastic <honhattienlul@gmail.com>
2024-01-01 16:26:13 +0800

    feat: updated sst.config.ts

--- sst.config.ts
+++ sst.config.ts
@@ -4,18 +4,20 @@ import { NextjsSite } from "sst/constructs";
 export default {
   config(_input) {
     return {
-      name: "pusher-research-project",
-      region: "us-east-1",
+      name: "aws-sst",
+      region: "ap-southeast-1",
     };
   },
   stacks(app) {
+    app.setDefaultRemovalPolicy("destroy");
     app.stack(function Site({ stack }) {
       const site = new NextjsSite(stack, "site", {
         environment: {
-          DATABASE_URL: process.env.DATABASE_URL,
+          ATABASE_URL: process.env.DATABASE_URL,
           NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-          GITHUB_ID: process.env.GITHUB_ID,
-          GITHUB_SECRET: process.env.GITHUB_SECRET,
+          NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+          NEXTAUTH_GITHUB_ID: process.env.NEXTAUTH_GITHUB_ID,
+          NEXTAUTH_GITHUB_SECRET: process.env.NEXTAUTH_GITHUB_SECRET,
           PUSHER_APP_ID: process.env.PUSHER_APP_ID,
           PUSHER_SECRET: process.env.PUSHER_SECRET,
           PUSHER_KEY: process.env.PUSHER_KEY,
