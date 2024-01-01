c13e6ccec130513ebff80829fb1e51ec918eacf0
Zolastic <honhattienlul@gmail.com>
2024-01-01 22:06:59 +0800

    feat: updated sst.config.ts and env.js

--- sst.config.ts
+++ sst.config.ts
@@ -1,10 +1,10 @@
-import { SSTConfig } from "sst";
+import type { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 
 export default {
   config(_input) {
     return {
-      name: "aws-sst",
+      name: "pusher-research-project",
       region: "ap-southeast-1",
     };
   },
@@ -13,15 +13,15 @@ export default {
     app.stack(function Site({ stack }) {
       const site = new NextjsSite(stack, "site", {
         environment: {
-          ATABASE_URL: process.env.DATABASE_URL,
-          NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-          NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-          NEXTAUTH_GITHUB_ID: process.env.NEXTAUTH_GITHUB_ID,
-          NEXTAUTH_GITHUB_SECRET: process.env.NEXTAUTH_GITHUB_SECRET,
-          PUSHER_APP_ID: process.env.PUSHER_APP_ID,
-          PUSHER_SECRET: process.env.PUSHER_SECRET,
-          PUSHER_KEY: process.env.PUSHER_KEY,
-          NEXT_PUBLIC_PUSHER_KEY: process.env.NEXT_PUBLIC_PUSHER_KEY,
+          DATABASE_URL: process.env.DATABASE_URL!,
+          NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET!,
+          NEXTAUTH_URL: process.env.NEXTAUTH_URL!,
+          NEXTAUTH_GITHUB_ID: process.env.NEXTAUTH_GITHUB_ID!,
+          NEXTAUTH_GITHUB_SECRET: process.env.NEXTAUTH_GITHUB_SECRET!,
+          PUSHER_APP_ID: process.env.PUSHER_APP_ID!,
+          PUSHER_SECRET: process.env.PUSHER_SECRET!,
+          PUSHER_KEY: process.env.PUSHER_KEY!,
+          NEXT_PUBLIC_PUSHER_KEY: process.env.NEXT_PUBLIC_PUSHER_KEY!,
         },
       });
 
